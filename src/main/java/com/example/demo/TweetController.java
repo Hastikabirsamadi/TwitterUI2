906768f27bba7a9ed55633eb663b3ddba0659a4b
hasti <hastikabirsamadi@gmail.com>
2023-06-26 22:00:34 +0330

    add tweet and main page controller

---
+++ src/main/java/com/example/demo/TweetController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import Model.Tweet;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+public class TweetController {
+
+    @FXML
+    private HBox tweetBox = new HBox();
+
+    public void showTweet(Tweet tweet) {
+        tweetBox
+    }
+}
