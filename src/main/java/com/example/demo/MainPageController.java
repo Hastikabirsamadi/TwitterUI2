207ab19f0854099a85c47eb97b99637fab19dfda
kiana <kiana.sherafati@gmail.com>
2023-06-27 21:10:14 +0330

    TweetController class

--- src/main/java/com/example/demo/MainPageController.java
+++ src/main/java/com/example/demo/MainPageController.java
@@ -1,6 +1,22 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 public class MainPageController {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox timeline;
+
+    @FXML
+    private ImageView profile;
+    @FXML
+    private ImageView search;
+    @FXML
+    private ImageView home;
 
     public void showTimeline(){
 
