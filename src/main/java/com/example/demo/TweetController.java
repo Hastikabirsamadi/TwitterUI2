4d26bdc8d9907782238aa245c0483b5f5f76344e
hasti <hastikabirsamadi@gmail.com>
2023-06-27 16:01:22 +0330

    tweet debug

--- src/main/java/com/example/demo/TweetController.java
+++ src/main/java/com/example/demo/TweetController.java
@@ -2,14 +2,33 @@ package com.example.demo;
 
 import Model.Tweet;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
-public class TweetController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TweetController implements Initializable {
 
     @FXML
     private HBox tweetBox = new HBox();
-
-    public void showTweet(Tweet tweet) {
-        tweetBox
+    @FXML
+    private Image image = new Image("like.png");
+    private ImageView likeIcon = new ImageView(new Image("like.png"));
+    @FXML
+    private Circle profileCircle;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        profileCircle.setFill(new ImagePattern(image));
     }
+    //    private VBox myBox;
+//    private void addBox(HBox hBox) {
+//        myBox.getChildren().add(hBox);
+//    }
+
 }
